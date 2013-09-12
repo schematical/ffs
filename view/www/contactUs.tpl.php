fec7f372c16ca47c3616e6af39d7f6f55449c0c2
Matt Lea <mlea@schematical.com>
2013-09-12 17:53:27 -0500

    fun with dates

--- view/www/contactUs.tpl.php
+++ view/www/contactUs.tpl.php
@@ -112,11 +112,11 @@
 </div> <!-- /container -->
 
 </div> <!-- /main -->
-<script type="text/javascript">
+<!--<script type="text/javascript">
     setTimeout(function(){var a=document.createElement("script");
         var b=document.getElementsByTagName("script")[0];
         a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0017/1061.js?"+Math.floor(new Date().getTime()/3600000);
         a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
-</script>
+</script>-->
 
 
